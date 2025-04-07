469ea1dad80976e19ca7bf86de4949e045c11d01
HeXed <100072714+HeXedek@users.noreply.github.com>
2025-04-07 22:24:16 +0200

    1

---
+++ startasnonadmin2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("conhost.exe",Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\ShitBlockar\\frontendmaybe.exe");
+
+        }
+    }
+}
